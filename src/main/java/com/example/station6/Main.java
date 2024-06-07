898d7bd3ca6442ede3fc163703cad01f691d4d54
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-06-08 00:30:21 +0900

    Station66

--- src/main/java/com/example/station6/Main.java
+++ src/main/java/com/example/station6/Main.java
@@ -3,6 +3,12 @@ package com.example.station6;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        int[] users = {23, 36, 31, 48, 58};
+        int all = 0;
+        for(int i = 0; i < users.length; i++) {
+            all += users[i];
+        }
+        System.out.println(all/5.0);
         // ここまで
     }
 }
